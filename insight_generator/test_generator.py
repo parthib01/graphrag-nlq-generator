59abd8abb7d2cb7ee926bdd0b83f928f62415eda
Parthib <parthibbanerjee01@gmail.com>
2025-07-07 19:35:22 +0530

    Just added another test case

--- insight_generator/test_generator.py
+++ insight_generator/test_generator.py
@@ -1,11 +1,16 @@
 from generator import generate_insight
 
 # Example user query
-user_query = "Show me the most reviewed product in July."
+#user_query = "Show me the most reviewed product in July."
+user_query = "Which region had the highest sales in August?"
+
 
 # Example SQL result
+# sql_result = [
+#     {"product_name": "Wireless Headphones", "total_reviews": 150}
+# ]
 sql_result = [
-    {"product_name": "Wireless Headphones", "total_reviews": 150}
+    {"region": "Mumbai", "total_sales": 58400.00}
 ]
 
 
